36d4e24afd5d4a2f94dfde9d4920488f6d220287
Max Kuh <maxes2805582@gmail.com>
2023-07-02 20:45:07 +0300

    Try refactor Price Li

---
+++ client/src/components/PriceItem/PriceLi/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './PriceLi.module.scss'
+function PriceLi(props) {
+  return (
+    <li  className={styles.liItem}>{value}</li>
+  )
+}
+
+export default PriceLi;
--- client/src/components/SelectPricing/index.jsx
+++ client/src/components/SelectPricing/index.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import styles from './Select.module.scss';
 function SelectPricing({ value, handleChange, selectPricingArr }) {
   const options = selectPricingArr.map((option) => (
-    <option key={option} value={option}>
+    <option key={option} value={option} className={styles.option}>
       {option}
     </option>
   ));
 
-  return (
-    <label className={styles.label}>
-      Pricing for
-      <select name="selectPricing" onChange={handleChange} value={value}>
-        {options}
-      </select>
-    </label>
+  return (  
+    <div className={styles.pricingWrapper}>
+    <label className={styles.label}>Pricing for</label>
+    <div className={styles.wrapper}>
+        <select name="selectPricing" onChange={handleChange} value={value} className={styles.select} >
+          {options} </select>
+    </div>
+    </div>
   );
 }
 
